db7fa99be462e2b2254ca4d44826bae8d5b41a3f
Ashu <ashu8306@gmail.com>
2018-06-30 04:02:34 +0530

    todo api

---
+++ server/models/todo.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+
+var Todo = mongoose.model('Todo',{
+   text: {
+     type: String,
+     required: true
+   },
+   completed: {
+     type: Boolean,
+     default: false
+   }
+});
+
+module.exports = {Todo};
---
+++ server/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+
+var port = process.env.PORT || 3000;
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+     console.log(req.body);
+     var todo = new Todo({
+       text: req.body.text
+     });
+     todo.save().then((doc) => {
+        res.send(doc);
+     },(e) => {
+        res.status(400).send();
+     });
+});
+
+app.get('/todos',(req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    },(e) => {
+      res.status(400).send(e);
+    });
+});
+
+app.listen(port, () => {
+   console.log(`started on port ${port}`);
+});
